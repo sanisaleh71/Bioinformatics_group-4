93d0782d11408b22620d36b304289c28c1e47401
sanisaleh71 <95065173+sanisaleh71@users.noreply.github.com>
2021-11-29 01:20:19 -0800

    Third Commit

---
+++ Script_umar.py
@@ -0,0 +1,4 @@
+print("NASIR UMAR ADAM")
+print("umarnaseer246@gmail.com")
+print("@Nasir U. Adam")
+print("GENOMICS")
---
+++ scripts_ahmad.py
@@ -0,0 +1,8 @@
+#<-----------Abdullahi Project------------>
+print("\t-------------BIOINFORMATICS SCRIPT---------------\n")
+
+print("\t\tHello Ahmad Bashir, below are your informations:\n")
+print("Name: " + "\t\t\t" + "Ahmad Bashir")
+print("Email: " + "\t\t\t"+ "ahmadmaje11@gmail.com")
+print("Slack_username: " + "\t" + "@ahmad_bashir")
+print("Area of interest: " + "\t" + "genomics")
