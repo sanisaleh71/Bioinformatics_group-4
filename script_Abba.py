7037a8bd2900fcbd2837f06a1916ae2eadb57a7a
sanisaleh71 <95065173+sanisaleh71@users.noreply.github.com>
2021-11-30 13:45:06 -0800

    Delete script_Abba.py

--- script_Abba.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python 
-#<------------Abba Project----------->
-print("\t----------BIOINFORMATICS SCRIPT---------\n")
-
-print("\t\Hello Abba Adamu, below are your informations:\n")
-print("Name: " + " \t\t\t" + "Abba_Adamu")
-print("Email: " + "\t\t\t"+ "abbaadamuhdj8@gmail.com")
-print("Area of interest: " + "\t" + "Genomics")
