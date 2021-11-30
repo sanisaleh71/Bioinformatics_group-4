a1564e04c25ac6acbf264044e2eed2bd7443d7e8
sanisaleh71 <95065173+sanisaleh71@users.noreply.github.com>
2021-11-30 13:40:59 -0800

    Delete SCRIPUMAR.java

--- SCRIPUMAR.java
+++
@@ -1,8 +0,0 @@
-public class SCRIPUMAR{
-	public static void main(String[]args){
-	System.out.println("Name: UMAR ALI BUBA");
-	System.out.println("Email: umarabkaragari@gmail.com");
-	System.out.println("Slack Id:@Umar Ali Buba");
-	System.out.println("Area of Interest: GENOMICS");
-	}
-}
