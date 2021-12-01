852f4b8fc9f24e93289308d7f315f6f2b5d989d3
Aishahumaira <95050695+Aishahumaira@users.noreply.github.com>
2021-12-01 14:54:08 +0100

    Add files via upload

---
+++ SCRIPTAISHA[1][1].java
@@ -0,0 +1,10 @@
+#!/usr/bin/env js
+public class SCRIPTAISHA{
+	public static void main(String[] args){
+		System.out.println("Name: AISHA IBRAHIM GAGARAU");
+		System.out.println("Email: aishaibrahimgagarau@gmail.com");
+		System.out.println("Slack Username: @AISHA IBRAHIM GAGARAU");
+		System.out.println("Area of Interest: GENOMICS");
+		return 0;
+	}
+}
