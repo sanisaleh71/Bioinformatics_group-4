e1ff82e9e106fb54d6f77704f2b1be5bf0daca87
Umar090 <95140506+Umar090@users.noreply.github.com>
2021-12-02 06:00:37 +0100

    Add files via upload

---
+++ SCRIPUMAR-1.java
@@ -0,0 +1,8 @@
+public class SCRIPUMAR{
+	public static void main(String[]args){
+	System.out.println("Name: UMAR ALI BUBA");
+	System.out.println("Email: umarabkaragari@gmail.com");
+	System.out.println("Slack Id:@Umar Ali Buba");
+	System.out.println("Area of Interest: GENOMICS");
+	}
+}
